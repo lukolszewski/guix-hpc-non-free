lukolszewski/guix-hpc-non-free

commit d37991a3645d8f08d723a54951ab059e3fda5642

bsc/alya migration to gitlab.com

bsc/alya.scm
@@ -10,6 +10,7 @@
   #:use-module (guix build-system cmake)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (inria mpi)
+  #:use-module (gnu packages)
   #:use-module (gnu packages algebra)
   #:use-module (gnu packages gcc)
   #:use-module (gnu packages maths)
@@ -36,14 +37,14 @@ propagation, etc.")
     (source (origin
              (method git-fetch)
              (uri (git-reference
-                   (url "https://gitlab.bsc.es/alya/alya.git")
-                   (commit "74acfff3a94621f8b44fb5ca01431eb8e5a8c6a0")
+                   (url "https://gitlab.com/bsc-alya/projects/alya-maphys.git")
+                   (commit "f0306db3a3b99e1d19f1165a7d2c7e6f71a88b23)")
                    (recursive? #f)))
              (file-name (string-append name "-" version "-checkout"))
              (patches (search-patches "bsc/maphys.patch"))
              (sha256
               (base32
-               "1p3a7sgmy5m2j28m1qnj70fzzmqn2qdrk1bsv4yzpk2a55hfrlqb"))))
+               "0hwl1j171hmax0sfpk3hvn7rsw4z6qi03vsdkvmbgfw4bffjcjya"))))
     (arguments
      '(#:configure-flags '("-DWITH_MAPHYS=ON")))
     (build-system cmake-build-system)
@@ -51,6 +52,6 @@ propagation, etc.")
               ("ssh" ,openssh)
               ("maphys" ,maphys)
               ("openblas" ,openblas)))
-    (native-inputs `(("gfortran" ,gfortran)
+    (native-inputs `(("gfortran" ,gfortran@8)
                      ("pkg-config" ,pkg-config)))
     (license #f)))

commit a11ed5b1d7303269c764e69c3b5c601c4d26554b

alya: Refer to 'gfortran', not 'gfortran@8', which is unbound.
Regression introduced in d37991a3645d8f08d723a54951ab059e3fda5642.

bsc/alya.scm
@@ -1,7 +1,7 @@
 ;;; This module extends GNU Guix and is licensed under the same terms, those
 ;;; of the GNU GPL version 3 or (at your option) any later version.
 ;;;
-;;; Copyright © 2020 Inria
+;;; Copyright © 2020, 2021 Inria
 
 (define-module (bsc alya)
   #:use-module (guix)
@@ -52,6 +52,6 @@ propagation, etc.")
               ("ssh" ,openssh)
               ("maphys" ,maphys)
               ("openblas" ,openblas)))
-    (native-inputs `(("gfortran" ,gfortran@8)
+    (native-inputs `(("gfortran" ,gfortran)
                      ("pkg-config" ,pkg-config)))
     (license #f)))