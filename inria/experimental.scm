c23b477f88f691aac93d1aeb647e4f0e501d19a4
Emmanuel Agullo <emmanuel.agullo@inria.fr>
2021-06-18 12:06:48 +0200

    qr_mumps moved to (free) guix-hpc

--- inria/experimental.scm
+++ inria/experimental.scm
@@ -4,6 +4,7 @@
 ;;; Copyright © 2019 Inria
 
 (define-module (inria experimental)
+  #:use-module (cnrs irit)
   #:use-module (guix)
   #:use-module (guix git-download)
   #:use-module ((guix licenses) #:prefix license:)
@@ -24,71 +25,6 @@
   #:use-module (guix utils)
   )
 
-(define-public qr_mumps
-  (package
-   (name "qr_mumps")
-   (version "a3027b41545bd419dbbcda0ca86c66359e05f6b5")
-   (home-page "http://buttari.perso.enseeiht.fr/qr_mumps/")
-   (source (origin
-            (method git-fetch)
-            (uri (git-reference
-                  (url "https://gitlab.com/qr_mumps/qr_mumps/")
-                  (commit version)
-		              ;; (commit (string-append "v" version))
-                  ;; We do not need using submodule for the moment
-                  ;; (recursive? #t)
-                  ))
-            (file-name (git-file-name name version))
-            (sha256
-             (base32
-              "1gzz2kzgwcc4ljdhlxnphwymhgb04nq5f125cirn651ad8lwk4l7"))))
-   (build-system cmake-build-system)
-   (arguments
-    '(#:configure-flags  (list
-                          "-Wno-dev"
-                          "-DBUILD_SHARED_LIBS=ON"
-                          (string-append "-DCMAKE_EXE_LINKER_FLAGS="
-                                         "-Wl,-rpath="
-                                         (assoc-ref %outputs "out")
-                                         "/lib")
-                          "-DQRM_WITH_STARPU=ON"
-                          "-DQRM_ORDERING_SCOTCH=ON"
-                          "-DQRM_ORDERING_METIS=ON"
-			                    "-DQRM_WITH_MPI=ON"
-                          "-DCMAKE_BUILD_TYPE=Release"
-                          )
-      #:phases (modify-phases %standard-phases
-                              (add-before 'check 'prepare-test-environment
-			                                    (lambda _
-				                                    (setenv "HOME" (getcwd)) ;; StarPU expects $HOME to be writable.
-	                                          (setenv "OMP_NUM_THREADS" "1")
-				                                    (setenv "OMPI_MCA_rmaps_base_oversubscribe" "1") #t)))
-      ;; No make test rule yet
-      #:tests? #f))
-   ;; TODO: refine which package shall be native, propagated, or regular input
-   (native-inputs `(("gfortran" ,gfortran)
-                    ("pkg-config" ,pkg-config)))
-   (inputs `(("metis" ,metis)
-             ("openblas" ,openblas)
-             ("perl" ,perl)
-             ("scotch32" ,scotch32)
-	           ("ssh" ,openssh)
-             ;; ("suitesparse" ,suitesparse) ;; for colamd; it would ideally be suitesparse:colamdonly
-             ("wget" ,wget)
-             ))
-   (propagated-inputs `(("starpu" ,starpu)))
-   (synopsis "Sparse QR direct solver (experimental package for distributed memroy version)")
-   (description
-    "qr_mumps is a software package for the solution of sparse, linear systems
-on multicore computers based on the QR factorization of the input matrix.
-Therefore, it is suited to solving sparse least-squares problems and to
-computing the minimum-norm solution of sparse, underdetermined problems. It can
-obviously be used for solving square problems in which case the stability
-provided by the use of orthogonal transformations comes at the cost of a higher
-operation count with respect to solvers based on, e.g., the LU factorization.
-qr_mumps supports real and complex, single or double precision arithmetic. This
-is an experimental version of the package for distributed memory." )
-   (license license:cecill)))
 
 (define-public qr_mumps+cuda
   (package
