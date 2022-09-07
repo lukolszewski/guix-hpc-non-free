506a9b9901f8a4eaba7d3a3cf9123240fc630194
Lukasz Olszewski <dev@lukaszolszewski.info>
2022-09-07 13:02:11 +0200

    tests removed

--- non-free/python-numpy-intel.scm
+++ non-free/python-numpy-intel.scm
@@ -317,26 +317,7 @@ include_dirs = ~:*~a/include~%" #$(this-package-input "mkl"))))))
               (substitute* "numpy/core/tests/test_cpu_features.py"
                 (("/bin/true") (search-input-file inputs "bin/true")))))
           (replace 'check
-            (lambda* (#:key tests? outputs inputs #:allow-other-keys)
-              (when tests?
-                (invoke "./runtests.py" "-vv" "--no-build" "--mode=fast"
-                        "-j" (number->string (parallel-job-count))
-                        ;; Contrary to scipy, the runtests.py script of numpy
-                        ;; does *not* automatically provide -n when -j is used
-                        ;; (see: https://github.com/numpy/numpy/issues/21359).
-                        "--" "-n" (number->string (parallel-job-count))
-                        "-k" (string-append
-                              ;; These tests may fail on 32-bit systems (see:
-                              ;; https://github.com/numpy/numpy/issues/18387).
-                              "not test_float_remainder_overflow "
-                              "and not test_pareto"
-                              ;; These tests seem to fail on machines without
-                              ;; an FPU is still under investigation upstream.
-                              ;; https://github.com/numpy/numpy/issues/20635
-                              #$@(if (target-riscv64?)
-                                   `(" and not test_float"
-                                     " and not test_fpclass")
-                                   '())))))))))
+            ))))
     (native-inputs
      (list python-cython
            python-hypothesis-next
